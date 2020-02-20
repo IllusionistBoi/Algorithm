27fa0bd8608826d8f64e893d2c69eb3a2524858c
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-02-20 14:42:26 +0000

    Computation Time Feature Added

--- Russian Peasant’s algorithm/src/com/rd/Main.java
+++ Russian Peasant’s algorithm/src/com/rd/Main.java
@@ -7,13 +7,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number for multiplication using the Russian Peasant’s algorithm");
         int Number1 = input.nextInt();
         int Number2 = input.nextInt();
 
-        System.out.println("Answer is : ");
-        System.out.println(Russian_PeasantAlgorithm.multiply(Number1,Number2));
+        System.out.println("Answer is : " + Russian_PeasantAlgorithm.multiply(Number1,Number2));
+
+        final long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Computation Time is : " + elapsedTime);
     }
 }
