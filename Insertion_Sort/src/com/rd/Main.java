38a745c6cc4295d6cc431e15c2536809bdc6d814
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-10 18:35:09 +0100

    Array printed and globalised

--- Insertion_Sort/src/com/rd/Main.java
+++ Insertion_Sort/src/com/rd/Main.java
@@ -2,7 +2,22 @@ package com.rd;
 
 public class Main {
 
+    // Made the array global so as to make it accessible from different function
+    private static int[] array;
+
+    /**
+     * Method to print the array passed
+     * @param array being printed
+     */
+    static void arrayPrint(int[] array) {
+        // Iterating through the element of array without using indices
+        for (int element : array)
+            System.out.println(element);
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
