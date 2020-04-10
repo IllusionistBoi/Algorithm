68e35a6c373f37d04a54a9dc1496c456f5048b35
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-11 00:03:03 +0100

    Comments & Bug removed

--- Insertion_Sort/src/com/rd/InsertionSort.java
+++ Insertion_Sort/src/com/rd/InsertionSort.java
@@ -1,21 +1,31 @@
 package com.rd;
+
 public class InsertionSort {
 
+    /**
+     * Method to sort the array using InsertionSort
+     *
+     * @param array unsorted array is passed
+     * @return sorted array is returned
+     */
     public Double[] sort (Double array[]) {
         Double key;
         int j;
-        int n = array.length;
 
-        for (int i = 1; i < n; ++i) {
+        // Iterated through entire array assigning key
+        for (int i = 1; i < array.length; ++i) {
             key = array[i];
             j = i -1;
 
+            // Comparing array value with key assigned
             while(j >= 0 && array[j] > key) {
                 array[j+1] = array[j];
                 j = j-1;
                 array[j+1] = key;
             }
         }
+
+        // Sorted array returned
         return array;
     }
 }
