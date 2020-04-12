4534c96fc7dccc10a60849eee9997b1f50a98e05
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-12 12:57:57 +0100

    error removed

--- Bogo_Sort/src/com/rd/Main.java
+++ Bogo_Sort/src/com/rd/Main.java
@@ -16,26 +16,26 @@ public class Main {
         // While loop to change the size of the array
         while(size < 1000) {
 
-            // Value assignment to the array using generatorArray function
+           // Value assignment to the array using generatorArray function
             array = new GeneratorArray((int) size).generate();
-//            System.out.println("Unsorted Array:");
-//            BogoSort.arrayPrint(array);
-//
-//            // To check the run time of the program
+            System.out.println("Unsorted Array:");
+            BogoSort.arrayPrint(array);
+
+            // To check the run time of the program
             final long startTime = System.currentTimeMillis();
-//            // Function Called with array passed as parameter
+            // Function Called with array passed as parameter
             BogoSort.RandomSort(array);
             final long elapsedTime = System.currentTimeMillis() - startTime;
-//
-//            System.out.println("Sorted Array is: ");
-//            BogoSort.arrayPrint(array);
 
-//            System.out.println("Computation Time is: " + elapsedTime + " for array size: " + size + " Is Sorted? ");
-//            System.out.println(BogoSort.isSorted(array));
-//            System.out.println(" ");
+            System.out.println("Sorted Array is: ");
+            BogoSort.arrayPrint(array);
+
+            System.out.println("Computation Time is: " + elapsedTime + " for array size: " + size + " Is Sorted? ");
+            System.out.println(BogoSort.isSorted(array));
+            System.out.println(" ");
 
             //Used for graph in excel
-            System.out.println(elapsedTime + " " + size + " " + BogoSort.isSorted(array));
+            //System.out.println(elapsedTime + " " + size + " " + BogoSort.isSorted(array));
 
             // changes the array input
             size = (int) (size * 1.2);
