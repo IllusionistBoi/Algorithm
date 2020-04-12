a08b0650d6e574da15fc0124a528fd6d95624d1a
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-12 16:22:16 +0100

    few error removed and documented

--- Quick_Sort/src/com/rd/Main.java
+++ Quick_Sort/src/com/rd/Main.java
@@ -3,16 +3,66 @@ package com.rd;
 import java.util.Arrays;
 
 public class Main {
-    private static int[] array;
+
+    // Made the array global so as to make it accessible from different function
+    private static Double[] array;
+
+    /**
+     * Method to print the array passed
+     * @param array being printed
+     */
+    static void arrayPrint(Double[] array) {
+        // Iterating through the element of array without using indices
+        for (Double element : array)
+            System.out.print(element + ", ");
+
+        System.out.println();
+    }
+
+    /**
+     * Method to check if the array is
+     * sorted or not
+     * @param array
+     * @return boolean true if sorted else false
+     */
+    static boolean isSorted(Double[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1])
+                return false;
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
-        // This is unsorted array
-        int[] array = new int[]{12, 13, 24, 10, 3, 6, 90, 70};
 
-        // Let's sort using quick sort
-        QuickSort.quickSort(array, 0, array.length - 1);
+        long size = 100;
 
-        // Verify sorted array
-        System.out.println(Arrays.toString(array));
+        // While loop to change the size of the array
+        while(size < 100000) {
+
+
+            // Value assignment to the array using generatorArray function
+            array = new GeneratorArray((int) size).generate();
+            System.out.println("Unsorted Array:");
+            arrayPrint(array);
+
+            // To check the run time of the program
+            final long startTime = System.nanoTime();
+
+            // Function Called with array passed as parameter
+            array = QuickSort.QuickSort(array, array.length);//sorting
+
+            final long elapsedTime = System.nanoTime() - startTime;
+
+            System.out.println("Computation Time is: " + elapsedTime + " for array size: " + size + " Is Sorted? " + isSorted(array) );
+            System.out.println(" ");
+
+            //Used for graph in excel
+            //System.out.println(elapsedTime + " " + size + " " + isSorted(array));
+
+            // changes the array input
+            size = (int) (size * 1.2);
+        }
     }
 }
--- Quick_Sort/src/com/rd/QuickSort.java
+++ Quick_Sort/src/com/rd/QuickSort.java
@@ -1,48 +1,76 @@
 package com.rd;
 
+import java.util.*;
+
 public class QuickSort {
 
-    public static int partition(int a[], int beg, int end) {
+    /**
+     * Array Partitioned and passing smaller array recursively
+     * then joining the sorted array returned form recursion and piviot
+     *
+     * @param array unsorted array passed
+     * @param arr_Size array size
+     * @return
+     */
+    public static Double[] QuickSort(Double[] array, int arr_Size) {
 
-        int left, right, temp, loc, flag;
-        loc = left = beg;
-        right = end;
-        flag = 0;
-        while (flag != 1) {
-            while ((a[loc] <= a[right]) && (loc != right))
-                right--;
-            if (loc == right)
-                flag = 1;
-            else if(a[loc] > a[right]) {
-                temp = a[loc];
-                a[loc] = a[right];
-                a[right] = temp;
-                loc = right;
-            }
+        // Base Case (Array Empty)
+        if(arr_Size < 2)
+            return array;
 
-            if (flag != 1) {
-                while ((a[loc] >= a[left]) && (loc != left))
-                    left++;
-                if (loc == left)
-                    flag = 1;
-                else if(a[loc] < a[left])
-                {
-                    temp = a[loc];
-                    a[loc] = a[left];
-                    a[left] = temp;
-                    loc = left;
-                }
+        // Pivot position
+        int current_position = 0;
+        Double temp;
+
+        // Dividing array into parts
+        for(int i = 1; i < arr_Size; i++) {
+            if(array[i] <= array[0]) {
+                current_position++;
+
+                temp = array[i];
+                array[i] = array[current_position];
+                array[current_position] = temp;
             }
         }
-        return loc;
-    }
 
-    static void quickSort(int a[], int beg, int end) {
-        int loc;
-        if (beg < end) {
-            loc = partition(a, beg, end);
-            quickSort(a, beg, loc - 1);
-            quickSort(a, loc + 1, end);
-        }
+        temp = array[0];
+
+        array[0] = array[current_position];
+
+        // Changing the pivot position with temp
+        array[current_position] = temp;
+
+        /**
+         * Sorting Left array elements
+         * Left position : With reference to pivot
+         */
+        Double[] left = QuickSort(array,current_position);
+
+        /**
+         *  Elements to the right of the pivot
+         *  separated in array2
+         */
+        Double[] array2 = Arrays.copyOfRange(array, current_position + 1, arr_Size);
+
+        /**
+         *  Sorting Right array elements
+         *  Right position : with reference to pivot
+         */
+        Double[] right = QuickSort(array2, arr_Size - current_position - 1);
+
+        /**
+         * Merging all the array
+         * @output final sorted array
+         */
+        Double[] final_array = new Double[arr_Size]; //final array, to merge everything together
+        for(int i = 0; i < current_position; i++)
+            final_array[i] = left[i];
+
+        final_array[current_position] = array[current_position];
+
+        for(int i = current_position + 1; i < arr_Size; i++)
+            final_array[i] = right[i - current_position - 1];
+
+        return final_array;
     }
 }
