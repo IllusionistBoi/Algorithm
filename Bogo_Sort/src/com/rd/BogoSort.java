111aae5bb488abbbaf8ffbbe88b48badc7cbf8f8
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-12 12:57:03 +0100

    error removed

--- Bogo_Sort/src/com/rd/BogoSort.java
+++ Bogo_Sort/src/com/rd/BogoSort.java
@@ -2,14 +2,69 @@ package com.rd;
 
 public class BogoSort {
 
+    private static Double[] array;
+
+    /**
+     * Method to print the array passed
+     *
+     * @param array being printed
+     */
+    public static void arrayPrint(Double[] array) {
+        // Iterating through the element of array without using indices
+        for (Double element : array)
+            System.out.print(element + ", ");
+
+        System.out.println();
+    }
+
+    /**
+     * Meethod to check if array is sorted or not
+     *
+     * @param a array to be sorted
+     * @return boolean true/false
+     */
+    static boolean isSorted(Double[] a) {
+        for (int i = 0; i < a.length - 1; ++i) {
+            if (a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Method to print number of times a sorting happens.
+     *
+     * @param arr
+     */
+
+    public static void RandomSort(Double[] arr) {
+        int counter = 0;
+        if (arr.length < 2) {
+            return;
+        }
+   //     System.out.println("We'll sort " + arr.length + " elements:");
+
+        // Till the time element are not sorted.
+        while (!isSorted(arr)) {
+            BogoSort.shuffle(arr);
+            counter++;
+        }
+
+        // when sorting done. we print the counter
+    //    System.out.println("The Solution found! (shuffled " + counter + " these many number of times)");
+   //     arrayPrint(arr);
+    }
+
     /**
      * Method to shuffle random indexes of given array
      *
      * @param arr array to be shuffled
      */
-    public static Double[] shuffle(Double[] arr) {
+    private static Double[] shuffle(Double[] arr) {
 
-        /* Takes two random index and assigns it to index 1 and 2 respectively. */
+        // Takes two random index and assigns it to index 1 and 2 respectively. /
         for (int x = 0; x < arr.length; ++x) {
             int index1 = (int) (Math.random() * arr.length);
             int index2 = (int) (Math.random() * arr.length);
--- Bogo_Sort/src/com/rd/Main.java
+++ Bogo_Sort/src/com/rd/Main.java
@@ -2,61 +2,11 @@ package com.rd;
 
 import java.util.Arrays;
 
-class RandomSort {
+public class Main {
 
     // Made the array global so as to make it accessible from different function
     private static Double[] array;
 
-    /**
-     * Method to print the array passed
-     * @param array being printed
-     */
-    static void arrayPrint(Double[] array) {
-        // Iterating through the element of array without using indices
-        for (Double element : array)
-            System.out.print(element + ", ");
-
-        System.out.println();
-    }
-
-    /**
-     * Method to print number of times a sorting happens.
-     * @param arr
-     */
-
-    public RandomSort(Double[] arr) {
-        int counter = 0;
-
-        System.out.println("We'll sort " + arr.length + " elements:");
-
-        // Till the time element are not sorted.
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] > array[i + 1]){
-                BogoSort.shuffle(arr);
-                counter++;
-            }
-        }
-
-        // when sorting done. we print the counter
-        System.out.println("The Solution found! (shuffled " + counter + " these many number of times)");
-        arrayPrint(arr);
-    }
-
-    /**
-     * Method to check if array is Sorted or not
-     * @param arr array passed
-     * @return boolean value
-     */
-    private static boolean isSorted(Double[] arr) {
-       // Iterated through the array and checked if previous value is greater or not.
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] >= arr[i - 1]) {
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
 
 
     public static void main(String[] args) {
@@ -64,27 +14,28 @@ class RandomSort {
         long size = 5;
 
         // While loop to change the size of the array
-        while(size < 10) {
+        while(size < 1000) {
 
             // Value assignment to the array using generatorArray function
             array = new GeneratorArray((int) size).generate();
-            System.out.println("Unsorted Array:");
-            arrayPrint(array);
-
-            // To check the run time of the program
+//            System.out.println("Unsorted Array:");
+//            BogoSort.arrayPrint(array);
+//
+//            // To check the run time of the program
             final long startTime = System.currentTimeMillis();
-            // Function Called with array passed as parameter
-            new RandomSort(array);
+//            // Function Called with array passed as parameter
+            BogoSort.RandomSort(array);
             final long elapsedTime = System.currentTimeMillis() - startTime;
+//
+//            System.out.println("Sorted Array is: ");
+//            BogoSort.arrayPrint(array);
 
-            System.out.println("Sorted Array is: ");
-            arrayPrint(array);
-
-            System.out.println("Computation Time is: " + elapsedTime + " for array size: " + size + " Is Sorted? " + isSorted(array));
-            System.out.println(" ");
+//            System.out.println("Computation Time is: " + elapsedTime + " for array size: " + size + " Is Sorted? ");
+//            System.out.println(BogoSort.isSorted(array));
+//            System.out.println(" ");
 
             //Used for graph in excel
-            //System.out.println(elapsedTime + " " + size + " " + isSorted(array));
+            System.out.println(elapsedTime + " " + size + " " + BogoSort.isSorted(array));
 
             // changes the array input
             size = (int) (size * 1.2);
