3bc26b8d182638a1d0ab55b3c245501992dc7187
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-04-21 14:41:23 +0100

    MergeSortEnhanced Algorithm written

--- Merge_Sort/src/com/rd/MergeSortEnhanced.java
+++ Merge_Sort/src/com/rd/MergeSortEnhanced.java
@@ -1,6 +1,28 @@
 package com.rd;
 
 public class MergeSortEnhanced {
+    /*
+        // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
+         private static void sort(double[] a, double[] aux, int lo, int hi) {
+        int n = hi - lo + 1;
+        if (n <= CUTOFF) {
+            insertionSort(a, lo, hi);
+            show(a, lo, hi);
+            return;
+        }
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+        show(a, lo, hi);
+    }
 
+    public static void sort(double[] a) {
+        double[] aux = new double[a.length];
+        sort(a, aux, 0, a.length-1);
+    }
+
+     */
 
 }
